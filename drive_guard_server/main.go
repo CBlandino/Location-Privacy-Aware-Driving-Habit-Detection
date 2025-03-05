eb4a2e3a26a5cc846453556fd7cb7573f10a2c61
rmcs9 <rmcswny@gmail.com>
2025-03-04 23:12:47 -0500

    impl simple auth

--- drive_guard_server/main.go
+++ drive_guard_server/main.go
@@ -3,18 +3,13 @@ package main
 
 import (
     "log"
-    "net/http"
     "github.com/gin-gonic/gin"
 )
 
-var Users []User
-var addr string
+var addr string = "localhost:8080"
 
 func main() {
-    addr = "localhost:6969"
     log.Println("STARTING SERVER")
-    Users = make([]User, 0)
-    log.Println("USER TABLE INITIALIZED")
     
     server := gin.Default()
     server.POST("/signup", signupUser) 
@@ -23,45 +18,4 @@ func main() {
     server.Run(addr)
 }
 
-func signupUser(c *gin.Context) {
-    log.Println("POST: signup user")
-    var newUser User 
-    err := c.BindJSON(&newUser)
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    log.Println("USER CREATED:", newUser.Email, "fname:", newUser.Firstname, "lname:", newUser.Lastname)
-
-    Users = append(Users, newUser)
-    c.IndentedJSON(http.StatusCreated, newUser)
-    log.Println("RESPONSE:", http.StatusCreated)
-}
-
-func loginUser(c *gin.Context) {
-    log.Println("POST: login user")
-    var loggedUser User 
-    err := c.BindJSON(&loggedUser)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for _, user := range Users {
-        if user.Email == loggedUser.Email && user.Password == loggedUser.Password {
-            c.IndentedJSON(http.StatusAccepted, user)
-            log.Println("USER FOUND: EMAIL:", user.Email, "RESPONSE:", http.StatusAccepted)
-            return
-        }
-    }
-
-    c.Status(http.StatusNotAcceptable)
-    log.Println("USER NOT FOUND. STATUS:", http.StatusNotAcceptable)
-}
-
-
-type User struct {
-    Email string `json:"email"`
-    Password string `json:"password"`
-    Firstname string `json:"first_name"`
-    Lastname string `json:"last_name"`
-}
