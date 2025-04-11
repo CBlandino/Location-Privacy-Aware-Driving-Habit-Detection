28999c5d9ac1d9af62e6d99441984f68cb9ce8d8
Jake Burleski <jakeburleski@gmail.com>
2025-04-11 00:10:07 -0400

    Implemented linear weighted function for scoring algorithm

--- drive_guard_server/score.go
+++ drive_guard_server/score.go
@@ -12,7 +12,7 @@ import (
 
 // come up with a mathemtical function i want for trip then implement into go
 // linear weighted function where the inputs are the number of breaks / seconds spent speeding
-// implement number of hard breaks, seconds spent speeding, number of times you sudden acceleration
+// implement number of hard breaks, seconds spent speeding, number of times you suddenly accelerate
 // maybe stick score in user table, can get user by looking up email for user
 
 // score is calculated off first trip then every subsequent trip changes that first score
@@ -30,6 +30,57 @@ func getUserScore(c *gin.Context, db *sql.DB) {
 // can process the 30 seconds blocks of data at a time or wait till all blocks are done (go with wait till all blocks)
 
 // argument is a map from string to int where each cell represents block of points, key will be long and lat, and pair will be time stamp
-func cacluateScore(pointsData []map[string]int) {
+
+// struct for driving habits
+type drivingHabit struct {
+	weight float64
+	input  float64
+}
+
+// constructor for driving habit, sets the weight and input
+func newHabit(weight float64, input float64) drivingHabit {
+	return drivingHabit{
+		weight: weight,
+		input:  input,
+	}
+}
+
+func calcuateScore(pointsData []float64) float64 {
+
+	// driving habits
+	var numBreaks float64
+	var numAccel float64
+	var timeSpeeding float64
+
+	harshBreak := newHabit(0.3, numBreaks)
+	harshAcceleration := newHabit(0.3, numAccel)
+	speeding := newHabit(0.4, timeSpeeding)
+
+	// thresholds for habits
+	if numBreaks <= 2 {
+		harshBreak.input = 1
+	}
+
+	if numAccel <= 2 {
+		harshAcceleration.input = 1
+	}
+
+	if timeSpeeding <= 10 {
+		speeding.input = 1
+	}
+
+	// Calculate final score
+	var totalWeighted float64
+	habits := []drivingHabit{harshBreak, harshAcceleration, speeding}
+	for _, habit := range habits {
+		totalWeighted += habit.weight * habit.input
+	}
+
+	score := 1.0 - totalWeighted
+	if score < 0 {
+		score = 0
+	}
+
+	return score
 
 }
