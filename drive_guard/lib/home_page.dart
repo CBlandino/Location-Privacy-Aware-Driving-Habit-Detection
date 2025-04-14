1dcc3a83d50620b1aa40e62968899de41827d924
CBlandino <christopherblandino0@gmail.com>
2025-04-14 14:30:11 -0400

    removed unnessesary code

--- drive_guard/lib/home_page.dart
+++ drive_guard/lib/home_page.dart
@@ -44,21 +44,6 @@ class _HomePageState extends State<HomePage> {
     late String firstName;
     late String lastName;
 
-  final List<Widget> _userPages = [
-    CurrentTripPage(),
-    PreviousTripsPage(),
-    ScorePage(),
-    SettingsPage(),
-  ];
-
-  final List<Widget> _insurancePages = [
-    // Placeholder pages for insurance provider
-    Container(), // User Lookup
-    Container(), // User Trips
-    Container(), // User Score
-    SettingsPage(),
-  ];
-
     void _onItemTapped(int index) {
       setState(() {
         _selectedIndex = index; // Switches pages     
