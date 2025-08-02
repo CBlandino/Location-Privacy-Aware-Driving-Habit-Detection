e48d41e399d40705e690cd0bed48dcbc71ef85d1
rmcs9 <rmcswny@gmail.com>
2025-08-02 17:21:50 -0400

    remove app code

--- drive_guard/android/app/src/main/kotlin/com/example/drive_guard/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.drive_guard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- drive_guard/lib/ipconfig.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  static String server = 'http://18.191.9.236:8080'; // Default to localhost
-
-  static void setServer(String newServer) {
-    server = newServer;
-  }
-}
